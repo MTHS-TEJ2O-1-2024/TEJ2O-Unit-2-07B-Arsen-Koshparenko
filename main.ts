7c6301e46a78a3f0943e5a564175cd931b8ab8e2
Arsen-Koshparenko <arsen.koshparenko@mths.ca>
2024-10-01 16:31:29 -0400

    Do right program

--- main.ts
+++ main.ts
@@ -1,8 +1,25 @@
-/* Copyright (c) 2020 MTHS All rights reserved
- *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+/* Copyright (c) 2020 MTHS All rights reserved 
+ * 
+ * Created by: Arsen 
+ * Created on: Oct 2024 
+ * This program add 1 if you press A 
 */
 
-basic.showString('Hello, World!')
+// setup 
+basic.clearScreen()
+let cookieNumber = 0
+basic.showIcon(IconNames.Happy)
+
+// turn on  
+input.onButtonPressed(Button.A, function () {
+    cookieNumber = cookieNumber + 1
+    basic.showNumber(cookieNumber)
+})
+
+// turn off 
+input.onButtonPressed(Button.B, function () {
+    cookieNumber = 0
+    basic.showNumber(cookieNumber)
+})
+
+
